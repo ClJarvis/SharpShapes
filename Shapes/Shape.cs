33d9ddef07ec1467b77e708dd6f5c760f526ba58
Chris_Jarvis <clj@ollure.com>
2015-08-17 21:12:31 -0500

    inital commit

---
+++ Shapes/Shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    public abstract class Shape : IShape
+    {
+        // we need info about colors
+        public float Area() { throw new NotImplementedException(); }
+        public float Perimeter() { throw new NotImplementedException(); }
+    }
+}
